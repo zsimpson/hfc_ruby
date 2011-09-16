c9844188465dd672ed6a2f50d37f06552bd75820
Zack Booth Simpson <oink54321@yahoo.com>
2011-09-16 12:32:02 -0700

    strarting on javascript refactoring

---
+++ public/javascripts/hfc.js
@@ -0,0 +1,303 @@
+
+
+function Hfc( options ) {
+	var frameComplete = true;
+	var frameNum = 0;
+	var doubleBuffer = false;
+	var $canvas[2] = { $("#"+options["canvas0"]), $("#"+options["canvas1"]) };
+
+	var restart = function( startCode, loopCode ) {
+		//
+		// Kills off any previous running thread and starts the thread again
+		//
+		
+		drawState.reset();
+
+		// CHECK if start code contains a clear, if so use double buffering
+		$canvas[1].css( "visibility", "hidden" );
+		doubleBuffer = loopCode.match( "^\\s*clear\\s*(\\s*)" ) ? true : false;
+
+		// DELETE the old thread
+		$.Hive.destroy();
+
+		frameNum = 0;
+
+
+??
+		defaultMat = $M( [
+			[ 1, 0, 0 ], 
+			[ 0, 1, 0 ], 
+			[ 0, 0, 1 ]
+		] ) 
+		context[0].setTransform( 1, 0, 0, 1, 0, 0 )
+		context[1].setTransform( 1, 0, 0, 1, 0, 0 )
+??
+
+		
+		// LAUNCH a new thread
+		$.Hive.create({
+			count: 1,
+			worker: '/javascripts/play_worker.js',
+			receive: function (data) {
+				if( data.cmd == "done" ) {
+					setErrorState( data.args[0], data.args[1], data.args[2] );
+					frameComplete = true;
+				}
+				else if( data.cmd == "functions" ) {
+					threadReturnedFunctions = data.args;
+				}
+				else if( data.cmd == "circle" ) {
+					drawState.loadStrokeState( context[frameNum%2] );
+					context[frameNum%2].beginPath();
+					context[frameNum%2].arc( data.args[0], data.args[1], data.args[2], 0, 3.14*2, false );
+					context[frameNum%2].closePath();
+					context[frameNum%2].stroke();
+				}
+				else if( data.cmd == "disc" ) {
+					drawState.loadFillState( context[frameNum%2] );
+					context[frameNum%2].beginPath();
+					context[frameNum%2].arc( data.args[0], data.args[1], data.args[2], 0, 3.14*2, false );
+					context[frameNum%2].closePath();
+					context[frameNum%2].fill();
+				}
+				else if( data.cmd == "line" ) {
+					drawState.loadStrokeState( context[frameNum%2] );
+					context[frameNum%2].beginPath();
+					context[frameNum%2].moveTo( data.args[0], data.args[1] );
+					context[frameNum%2].lineTo( data.args[2], data.args[3] );
+					context[frameNum%2].stroke();
+				}
+				else if( data.cmd == "arc" ) {
+					drawState.loadStrokeState( context[frameNum%2] );
+					context[frameNum%2].beginPath();
+					context[frameNum%2].arc( data.args[0], data.args[1], data.args[2], data.args[3], data.args[4], data.args[5] );
+					context[frameNum%2].stroke();
+				}
+				else if( data.cmd == "box" ) {
+					drawState.loadStrokeState( context[frameNum%2] );
+					context[frameNum%2].strokeRect( data.args[0], data.args[1], data.args[2], data.args[3] );
+				}
+				else if( data.cmd == "rect" ) {
+					drawState.loadFillState( context[frameNum%2] );
+					context[frameNum%2].fillRect( data.args[0], data.args[1], data.args[2], data.args[3] );
+				}
+				else if( data.cmd == "text" ) {
+					drawState.loadFillState( context[frameNum%2] );
+					drawState.loadFontState( context[frameNum%2] );
+					context[frameNum%2].fillText( data.args[0], data.args[1], data.args[2] );
+				}
+				else if( data.cmd == "stroketext" ) {
+					drawState.loadStrokeState( context[frameNum%2] );
+					drawState.loadFontState( context[frameNum%2] );
+					context[frameNum%2].strokeText( data.args[0], data.args[1], data.args[2] );
+				}
+				else if( data.cmd == "font" ) {
+					drawState.setFont( data.args[0], data.args[1] );
+				}
+				else if( data.cmd == "align" ) {
+					drawState.setAlign( data.args[0], data.args[1] );
+				}
+
+				else if( data.cmd == "fill" ) {
+					drawState.setFill( data.args[0], data.args[1], data.args[2], data.args[3] );
+				}
+				else if( data.cmd == "stroke" ) {
+					drawState.setStroke( data.args[0], data.args[1], data.args[2], data.args[3], data.args[4], data.args[5], data.args[6] );
+				}
+				else if( data.cmd == "shadow" ) {
+					drawState.setShadow( data.args[0], data.args[1], data.args[2], data.args[3], data.args[4], data.args[5], data.args[6] );
+				}
+
+				else if( data.cmd == "clear" ) {
+					context[frameNum%2].setTransform( 1, 0, 0, 1, 0, 0 );
+					context[frameNum%2].fillStyle = colorToString( data.args[0], data.args[1], data.args[2] );
+					context[frameNum%2].globalAlpha = 1;
+					context[frameNum%2].fillRect( 0, 0, canvasW, canvasH );
+				}
+				else if( data.cmd == "window" ) {
+					defaultMat = $M( [
+						[ data.args[0], data.args[2], data.args[4] ], 
+						[ data.args[1], data.args[3], data.args[5] ], 
+						[ 0, 0, 1 ]
+					] ) 
+					context[frameNum%2].setTransform( data.args[0], data.args[1], data.args[2], data.args[3], data.args[4], data.args[5] );
+				}
+				else if( data.cmd == "identity" ) {
+					context[frameNum%2].setTransform( defaultMat.e(1,1), defaultMat.e(2,1), defaultMat.e(1,2), defaultMat.e(2,2), defaultMat.e(1,3), defaultMat.e(2,3) );
+				}
+				else if( data.cmd == "translate" ) {
+					context[frameNum%2].translate( data.args[0], data.args[1] );
+				}
+				else if( data.cmd == "scale" ) {
+					context[frameNum%2].scale( data.args[0], data.args[1] );
+				}
+				else if( data.cmd == "rotate" ) {
+					context[frameNum%2].rotate( data.args[0] );
+				}
+				else if( data.cmd == "push" ) {
+					context[frameNum%2].save();
+				}
+				else if( data.cmd == "pop" ) {
+					context[frameNum%2].restore();
+				}
+				
+				else if( data.cmd == "beginPath" ) {
+					context[frameNum%2].beginPath();
+				}
+				else if( data.cmd == "closePath" ) {
+					context[frameNum%2].closePath();
+				}
+				else if( data.cmd == "moveTo" ) {
+					context[frameNum%2].moveTo( data.args[0], data.args[1] );
+				}
+				else if( data.cmd == "lineTo" ) {
+					context[frameNum%2].lineTo( data.args[0], data.args[1] );
+				}
+				else if( data.cmd == "curveTo" ) {
+					context[frameNum%2].quadraticCurveTo( data.args[0], data.args[1], data.args[2], data.args[3] );
+				}
+				else if( data.cmd == "bezierTo" ) {
+					context[frameNum%2].bezierCurveTo( data.args[0], data.args[1], data.args[2], data.args[3], data.args[4], data.args[5] );
+				}
+				else if( data.cmd == "arcTo" ) {
+					context[frameNum%2].arcTo( data.args[0], data.args[1], data.args[2], data.args[3], data.args[4] );
+				}
+				else if( data.cmd == "pathArc" ) {
+					context[frameNum%2].arc( data.args[0], data.args[1], data.args[2], data.args[3], data.args[4], data.args[5] );
+				}
+				else if( data.cmd == "strokePath" ) {
+					drawState.loadStrokeState( context[frameNum%2] );
+					context[frameNum%2].stroke();
+				}
+				else if( data.cmd == "fillPath" ) {
+					drawState.loadFillState( context[frameNum%2] );
+					context[frameNum%2].fill();
+				}
+				else if( data.cmd == "alpha" ) {
+					drawState.setAlpha( data.args[0] );
+				}
+				else if( data.cmd == "composite" ) {
+					drawState.setComposite( data.args[0] );
+				}
+				else if( data.cmd == "gradient" ) {
+					drawState.createGradient( data.args[0], data.args[1], data.args[2], data.args[3] );
+				}
+				else if( data.cmd == "gradientRadial" ) {
+					drawState.createGradientRadial( data.args[0], data.args[1], data.args[2], data.args[3], data.args[4], data.args[5] );
+				}
+				else if( data.cmd == "addColorStop" ) {
+					drawState.addColorStop( data.args[0], data.args[1], data.args[2], data.args[3] );
+				}
+				else if( data.cmd == "debug" ) {
+					$("#debugStream").append( data.args[0] + "<br/>" );
+					$("#debugStream").scrollTop($("#debugStream")[0].scrollHeight);
+					console.log( data.args[0] );
+				}
+
+				else if( data.cmd == "image" ) {
+					var i = images[ data.args[0] ];
+					if( ! i ) {
+						i = new Image();
+						if( data.args[0].match( /http(s)?:/ ) ) {
+							i.src = data.args[0];
+						}
+						else {
+							i.src = "/home/get_asset?name=" + data.args[0];
+						}
+						i.onload = getImageDims;
+						images[ data.args[0] ] = i;
+					}
+					if( data.args[5] != null ) {
+						context[frameNum%2].drawImage( i, data.args[5], data.args[6], data.args[7], data.args[8], data.args[1], data.args[2], data.args[3], data.args[4] );
+					}
+					else if( data.args[3] != null ) {
+						context[frameNum%2].drawImage( i, data.args[1], data.args[2], data.args[3], data.args[4] );
+					}
+					else {
+						context[frameNum%2].drawImage( i, data.args[1], data.args[2] );
+					}
+				}
+				
+				else if( data.cmd == "unresponsiveOverride" ) {
+					unresponsiveOverride = true
+				}
+			}
+		});
+
+		codeBlocks = [];
+		codeBlockNames = [];
+		for( var i in publicFunctions ) {
+			codeBlockNames.push( i );
+			codeBlocks.push( publicFunctions[i] );
+		}
+		codeBlocks.push( startCode );
+		codeBlockNames.push( "Start code" );
+		
+		setErrorState( "", "", "" );
+		$.Hive.get(0).send( [codeBlockNames,codeBlocks,null,0,0,false,null,null,imageDims,false] );
+
+		frameComplete = false;
+	}
+	}
+
+	var runFrame = function( state ) {
+		// Tells the thread to run one frame
+	}
+}
+
+
+
+
+/*
+var savePower = false;
+
+var frameComplete = true;
+var frameNum = 0;
+var context = [];
+var canvasW = 350;
+var canvasH = 350;
+var mouseX=0, mouseY=0, mouseDown=false;
+var keyDown=null;
+var doubleBuffer = false;
+
+var drawState = new DrawState();
+var images = [];
+
+var challengeStartCode = null;
+var challengeLoopCode = null;
+
+var startFrameTime;
+var unresponsiveOverride = false
+
+var defaultMat = $M( [
+
+imageDims = {};
+function getImageDims() {
+
+var errorInLoopCode = false;
+function setErrorState( blockName, exceptMessage, exceptLine ) {
+
+function colorToString( r, g, b ) {
+
+function DrawState() {
+
+
+
+var frameRates = [];
+var count = 0
+var stopWaitTime;
+var startWaitTime;
+
+
+
+// public functions
+//===================================================================================
+
+var publicFunctions = [];
+var publicFunctionsLoad = [];
+var publicFunctionsEditting = "";
+
+function clearFunctions() {
+function updateFunctionsCode() {
+function bindFunction( name ) {
+*/
---
+++ public/javascripts/hfc_run_worker.js
@@ -0,0 +1,411 @@
+importScripts('/javascripts/jquery.hive.pollen.js');
+importScripts('/javascripts/sylvester.js');
+with( Math ) {
+
+	var _keyDown, _mouseX, _mouseY, _mouseDown, _imageDims;
+	var _defaultMat = Matrix.I( 3 );
+	var _transform = Matrix.I( 3 );
+
+	// runFrame:
+	//   This function is called by the web-worker threading interface once per frame.
+	/    Because these threads can not talk to the DOM directly there is quite of bit
+	//   of messaging that has to pass state back and forth.
+	//==========================================================================================
+	$(function( data ) {
+
+		var codeBlockNames = data.message[0];
+		var codeBlocks = data.message[1];
+		_keyDown = data.message[2];
+		_mouseX = data.message[3];
+		_mouseY = data.message[4];
+		_mouseDown = data.message[5];
+		searchForSymbols = data.message[6];
+		twiddlers = data.message[7];
+		_imageDims = data.message[8];
+		var isLoopCode = data.message[9];
+		if( isLoopCode ) {
+			_transform = Matrix.I(3);
+		}
+
+		if( twiddlers ) {
+			for( var i=0; i<twiddlers.length; i+=2 ) {
+				self[ twiddlers[i] ] = twiddlers[i+1]
+			}
+		}
+    
+		if( searchForSymbols ) {
+
+			var returnSymbols = { "fakeSymbol":"" };
+				// The fakeSymbol is there so that SOMETHING is returned
+
+			for( var __p in self ) {
+				if( __p.match( /^\$/ ) && __p != "$" ) {
+					returnSymbols[__p.toString()] = self[__p].toString();
+				}
+			}
+
+			$.send( { "cmd":"functions", "args":returnSymbols } );
+			return;
+		}
+
+		var i;
+		var exceptMessage = null;
+		var exceptLine = 0;
+		var blockName = null;
+		try {
+			for( i=0; i<codeBlocks.length; i++ ) {
+				blockName = codeBlockNames[i];
+				eval( codeBlocks[i] );
+			}
+		}
+		catch( e ) {
+			exceptMessage = e.message;
+			exceptLine = e.lineNumber - 20;
+		}
+
+		$.send( { "cmd":"done", "args":[blockName,exceptMessage,exceptLine] } );
+	});
+
+	// IO
+	//=======================================================================================
+
+	function keyDown() {
+		if( _keyDown ) {
+			return String.fromCharCode( _keyDown );
+		}
+		return null;
+	}
+
+	function mouseX() {
+		return _mouseX;
+	}
+
+	function mouseY() {
+		return _mouseY;
+	}
+
+	function mouseDown() {
+		return _mouseDown;
+	}
+
+	function time() {
+		return new Date().getTime();
+	}
+
+	function debug( str ) {
+		$.send( { "cmd":"debug", "args":[str] } );
+	}
+
+	function unresponsiveOverride() {
+		$.send( { "cmd":"unresponsiveOverride" } )
+	}
+
+	function delay( t ) {
+		var start = new Date().getTime();
+		while( true ) {
+			var now = new Date();
+			if( now.getTime() - start > t ) {
+				break;
+			}
+		}
+	}
+
+	// Math
+	//=======================================================================================
+
+	function rand(x,y) {
+		if( typeof(x) == "undefined" ) {
+			x = 0;
+			y = 1;
+		}
+		if( typeof(y) == "undefined" ) {
+			y = x;
+			x = 0;
+		}
+		return Math.random()*(y-x)+x;
+	}
+
+	function randi(x,y) {
+		if( typeof(x) == "undefined" ) {
+			x = 0;
+			y = 2;
+		}
+		if( typeof(y) == "undefined" ) {
+			y = x;
+			x = 0;
+		}
+		return Math.floor( Math.random()*(y-x)+x );
+	}
+
+	function pi() {
+		return Math.PI;
+	}
+
+	function pi2() {
+		return 2*Math.PI;
+	}
+
+	function e() {
+		return Math.E;
+	}
+
+	function distance( x0, y0, x1, y1 ) {
+		dx = x1-x0;
+		dy = y1-y0;
+		return Math.sqrt( dx*dx + dy*dy );
+	}
+
+	// Graphics
+	//=======================================================================================
+
+	function width() {
+		return 350;
+	}
+
+	function height() {
+		return 350;
+	}
+
+	function cenX() {
+		return 175;
+	}
+
+	function cenY() {
+		return 175;
+	}
+
+	function fill( r, g, b, a ) {
+		if( typeof(r) == "undefined" ) r = 0;
+		if( typeof(g) == "undefined" ) g = 0;
+		if( typeof(b) == "undefined" ) b = 0;
+		if( typeof(a) == "undefined" ) a = 1;
+		$.send( { "cmd":"fill", "args":[r, g, b, a] } );
+	}
+
+	function shadow( x, y, blur, r, g, b ) {
+		if( typeof(blur) == "undefined" ) blur = 2;
+		if( typeof(r) == "undefined" ) r = 0;
+		if( typeof(g) == "undefined" ) g = 0;
+		if( typeof(b) == "undefined" ) b = 0;
+		$.send( { "cmd":"shadow", "args":[x, y, blur, r, g, b] } );
+	}
+
+	function circle( x, y, r ) {
+		$.send( { "cmd":"circle", "args":[x,y,r] } );
+	}
+
+	function disc( x, y, r ) {
+		$.send( { "cmd":"disc", "args":[x,y,r] } );
+	}
+
+	function line( x0, y0, x1, y1 ) {
+		$.send( { "cmd":"line", "args":[x0,y0,x1,y1] } );
+	}
+
+	function arc( cx, cy, r, a0, a1, ac ) {
+		if( typeof(ac) == "undefined" ) ac = false;
+		$.send( { "cmd":"arc", "args":[cx,cy,r,a0,a1,ac] } );
+	}
+
+	function pathBegin() {
+		$.send( { "cmd":"beginPath" } );
+	}
+
+	function pathClose() {
+		$.send( { "cmd":"closePath" } );
+	}
+
+	function pathMoveTo( x, y ) {
+		$.send( { "cmd":"moveTo", "args":[x,y] } );
+	}
+
+	function pathLineTo(x,y) {
+		$.send( { "cmd":"lineTo", "args":[x,y] } );
+	}
+
+	function pathCurveTo(cx,cy,x,y) {
+		$.send( { "cmd":"curveTo", "args":[cx,cy,x,y] } );
+	}
+
+	function pathBezierTo(cx0,cy0,cx1,cy1,x,y) {
+		$.send( { "cmd":"bezierTo", "args":[cx0,cy0,cx1,cy1,x,y] } );
+	}
+
+	function pathArcTo(x0,y0,x1,y1,r) {
+		$.send( { "cmd":"arcTo", "args":[x0,y0,x1,y1,r] } );
+	}
+
+	function pathArc(x,y,r,a0,a1,antiClock) {
+		if( typeof(antiClock) == "undefined" ) antiClock = false;
+		$.send( { "cmd":"pathArc", "args":[x,y,r,a0,a1,antiClock] } );
+	}
+
+	function pathStroke() {
+		$.send( { "cmd":"strokePath" } );
+	}
+
+	function pathFill() {
+		$.send( { "cmd":"fillPath" } );
+	}
+
+	function gradient( x0, y0, x1, y1 ) {
+		$.send( { "cmd":"gradient", "args":[x0,y0,x1,y1] } );
+	}
+
+	function gradientRadial( x0, y0, r0, x1, y1, r1 ) {
+		$.send( { "cmd":"gradientRadial", "args":[x0,y0,r0,x1,y1,r1] } );
+	}
+
+	function gradientAddColor( p, r, g, b ) {
+		$.send( { "cmd":"addColorStop", "args":[p,r,g,b] } );
+	}
+
+	function stroke( w, r, g, b, a, cap, join ) {
+		if( typeof(w) == "undefined" ) w = 1;
+		if( typeof(r) == "undefined" ) r = 0;
+		if( typeof(g) == "undefined" ) g = 0;
+		if( typeof(b) == "undefined" ) b = 0;
+		if( typeof(a) == "undefined" ) a = 1;
+		if( typeof(cap) == "undefined" ) cap="butt";
+		if( typeof(join) == "undefined" ) cap="miter";
+		$.send( { "cmd":"stroke", "args":[w,r,g,b,a,cap,join] } );
+	}
+
+	function clear( r, g, b ) {
+		if( typeof(r) == "undefined" ) r = 1;
+		if( typeof(g) == "undefined" ) g = 1;
+		if( typeof(b) == "undefined" ) b = 1;
+		$.send( { "cmd":"clear", "args":[r,g,b] } );
+	}
+
+	function box( x, y, w, h ) {
+		$.send( { "cmd":"box", "args":[x,y,w,h] } );
+	}
+
+	function rect( x, y, w, h ) {
+		$.send( { "cmd":"rect", "args":[x,y,w,h] } );
+	}
+
+	function alpha( a ) {
+		$.send( { "cmd":"alpha", "args":[a] } );
+	}
+
+	function imageWidth( name ) {
+		if( typeof _imageDims != "undefined" && typeof _imageDims[name] != "undefined" ) {
+			return _imageDims[name][0];
+		}
+		return 0;
+	}
+
+	function imageHeight( name ) {
+		if( typeof _imageDims != "undefined" && typeof _imageDims[name] != "undefined" ) {
+			return _imageDims[name][1];
+		}
+		return 0;
+	}
+
+	function image( i, x, y, w, h, sx, sy, sw, sh ) {
+		if( typeof(x) == "undefined" ) x = 0;
+		if( typeof(y) == "undefined" ) y = 0;
+		$.send( { "cmd":"image", "args":[i,x,y,w,h,sx,sy,sw,sh] } );
+	}
+
+	function text( a, x, y ) {
+		if( typeof(x) == "undefined" ) x = 0;
+		if( typeof(y) == "undefined" ) y = 0;
+		$.send( { "cmd":"text", "args":[a,x,y] } );
+	}
+
+	function stroketext( a, x, y ) {
+		if( typeof(x) == "undefined" ) x = 0;
+		if( typeof(y) == "undefined" ) y = 0;
+		$.send( { "cmd":"stroketext", "args":[a,x,y] } );
+	}
+
+	function font( size, name ) {
+		if( typeof(name) == "undefined" ) name = "Calibri";
+		if( typeof(size) == "undefined" ) size = 20;
+		$.send( { "cmd":"font", "args":[name,size] } );
+	}
+
+	function align( align, baseline ) {
+		if( typeof(align) == "undefined" ) align = "left";
+		if( typeof(baseline) == "undefined" ) baseline = "top";
+		$.send( { "cmd":"align", "args":[align,baseline] } );
+	}
+
+	function window( x0,y0, x1,y1 ) {
+		var sx = width() / (x1-x0)
+		var sy = height() / (y1-y0)
+		_defaultMat = $M( [
+			[ sx, 0, -x0*sx ], 
+			[ 0, sy, -y0*sy ], 
+			[ 0, 0, 1 ]
+		] ) 
+		_transform = _defaultMat
+		$.send( { "cmd":"window", "args":[sx,0,0,sy,-x0*sx,-y0*sy] } );
+	}
+
+	function identity() {
+		_transform = _defaultMat
+		$.send( { "cmd":"identity" } );
+	}
+
+	function translate( x, y ) {
+		var m = $M( [
+			[ 1, 0, x ], 
+			[ 0, 1, y ], 
+			[ 0, 0, 1 ]
+		] ); 
+		_transform = _transform.multiply( m ); 
+		$.send( { "cmd":"translate", "args":[x,y] } );
+	}
+
+	function scale( x, y ) {
+		if( typeof(y) == "undefined" ) y = x;
+		var m = $M( [
+			[ x, 0, 0 ], 
+			[ 0, y, 0 ], 
+			[ 0, 0, 1 ]
+		] ); 
+		_transform = _transform.multiply( m ); 
+		$.send( { "cmd":"scale", "args":[x,y] } );
+	}
+
+	function rotate( a ) {
+		var c = Math.cos( a )
+		var s = Math.sin( a )
+		var m = $M( [
+			[ c,-s, 0 ], 
+			[ s, c, 0 ], 
+			[ 0, 0, 1 ]
+		] ); 
+		_transform = _transform.multiply( m ); 
+		$.send( { "cmd":"rotate", "args":[a] } );
+	}
+
+	function project( x, y ) {
+		var o = _transform.multiply( $V([x,y,1]) )
+		return { x:o.elements[0], y:o.elements[1] }
+	}
+
+	function unproject( x, y ) {
+		var inv = _transform.inverse();
+		var o = inv.multiply( $V([x,y,1]) )
+		return { x:o.elements[0], y:o.elements[1] }
+	}
+
+	function pushState() {
+		$.send( { "cmd":"push" } );
+	}
+
+	function popState() {
+		$.send( { "cmd":"pop" } );
+	}
+
+
+}
+
+
+
+
