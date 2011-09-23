b7a62884631ed9fef4e7f65c95201bf3da0e526d
Zack Booth Simpson <oink54321@yahoo.com>
2011-09-23 16:38:06 -0700

    Added IE message

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,21 @@
 class HomeController < ApplicationController
 	def index
+		if @is_ie
+			render :text=>"<h2>Happy Fun Coding does not work with Internet Explorer because IE does not support the latest web standards.<br/><br/>Please try Chrome, Firefox, or Safari.</h2>"
+			return
+		else
 		@program_to_load = params[:id]
 	end
 	
 	def embed
+		if @is_ie
+			render :text=>"Happy Fun Coding does not work with Internet Explorer because IE does not support the latest web standards.<br/><br/>Please try Chrome, Firefox, or Safari."
+			return
+		else
 		@program_to_load = params[:id]
 		render :layout=>false
 	end
 end
+
+
+			
